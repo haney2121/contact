b8a14d248ce9e9e73b832ca5c4b61574a232c9c1
Haney2121 <haney037332@gmail.com>
2018-10-15 14:36:40 -0500

    added logic for the submit form and error inputs

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,65 +1,95 @@
 import React, { Component, Fragment } from "react";
+import { Consumer } from "../../context";
+import TextInputGroup from "../layout/TextInputGroup";
+import uuid from "uuid";
 
 class AddContact extends Component {
   state = {
     name: "",
     email: "",
-    phone: ""
+    phone: "",
+    errors: {}
   };
 
-  onSubmit = e => {
+  onSubmit = (dispatch, e) => {
     e.preventDefault();
-    console.log(this.state);
+    const { name, email, phone } = this.state;
+    //Check for Errors
+    if (name === "") {
+      this.setState({ errors: { name: "Name is required" } });
+      return;
+    }
+    if (email === "") {
+      this.setState({ errors: { email: "E-mail is required" } });
+      return;
+    }
+    if (phone === "") {
+      this.setState({ errors: { phone: "Phone is required" } });
+      return;
+    }
+    const newContact = { name, email, phone, id: uuid() };
+    dispatch({ type: "ADD_CONTACT", payload: newContact });
+    this.setState({
+      name: "",
+      email: "",
+      phone: "",
+      errors: {}
+    });
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
     return (
-      <Fragment>
-        <div className="card mb-3">
-          <div className="card-header">Add Contact</div>
-          <div className="card-body">
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input
-                  type="text"
-                  className="form-control form-control-lg"
-                  placeholder="Enter Name..."
-                  name="name"
-                  value={name}
-                  onChange={this.onChange}
-                />
-                <label htmlFor="email">E-mail</label>
-                <input
-                  type="email"
-                  className="form-control form-control-lg"
-                  placeholder="Enter E-mail..."
-                  name="email"
-                  value={email}
-                  onChange={this.onChange}
-                />
-                <label htmlFor="phone">Phone</label>
-                <input
-                  type="text"
-                  className="form-control form-control-lg"
-                  placeholder="Enter Phone Number..."
-                  name="phone"
-                  value={phone}
-                  onChange={this.onChange}
-                />
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <Fragment>
+              <div className="card mb-3">
+                <div className="card-header">Add Contact</div>
+                <div className="card-body">
+                  <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                    <TextInputGroup
+                      name="name"
+                      type="text"
+                      placeholder="Enter Name..."
+                      value={name}
+                      onChange={this.onChange}
+                      label="Name"
+                      error={errors.name}
+                    />
+                    <TextInputGroup
+                      name="email"
+                      type="email"
+                      placeholder="Enter E-mail..."
+                      value={email}
+                      onChange={this.onChange}
+                      label="E-mail"
+                      error={errors.email}
+                    />
+                    <TextInputGroup
+                      name="phone"
+                      type="text"
+                      placeholder="Enter Phone..."
+                      value={phone}
+                      onChange={this.onChange}
+                      label="Phone"
+                      error={errors.phone}
+                    />
+                    <input
+                      type="submit"
+                      value="Add Contact"
+                      className="btn btn-light btn-block"
+                    />
+                  </form>
+                </div>
               </div>
-              <input
-                type="submit"
-                value="Add Contact"
-                className="btn btn-light btn-block"
-              />
-            </form>
-          </div>
-        </div>
-      </Fragment>
+            </Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
